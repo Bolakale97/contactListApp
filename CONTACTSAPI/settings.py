e33f38d0fef8a512fb04e428e96cc5936d2ee3b8
Bolakale97 <ahmedibrahimiyanda10@gmail.com>
2023-09-29 18:08:52 +0100

    test

--- CONTACTSAPI/settings.py
+++ CONTACTSAPI/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-^&ve+^8jq^h^-k$a$vqm)xl36zrrqi6$*#*khff*tcz%nr-#nk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition

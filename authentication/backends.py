87d790f9d29f74efea00376633aa1570cc8d3a35
Bolakale97 <ahmedibrahimiyanda10@gmail.com>
2023-10-03 15:57:35 +0100

    test

--- authentication/backends.py
+++ authentication/backends.py
@@ -14,7 +14,7 @@ class JWTAuthentication(authentication.BaseAuthentication):
         prefix,token = auth_data.decode('utf-8').split(' ', maxsplit=1)
 
         try:
-            payload = pyjwt.decode(token, settings.JWT_SECRET_KEY, algorithms=['HS256'])
+            payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=['HS256'])
 
             user = User.objects.get(username=payload['username'])
             return (user, token)
